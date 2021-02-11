b8d7136cea1cf09b9d742bbc69da7b9555d4655a
salimsorker1122 <salimsorker1122@gmail.com>
2021-02-12 01:38:29 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# health-city
---
+++ js/slick.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+$(".slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrows: false,
+});
+});
+
+$(document).ready(function(){
+$(".slider-reviews").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrows: false,
+});
+});
+
